9e00487038bcba115d244a8b89e1b0b454b47ad0
Rosa Pérez <rosaperez@iMac.local>
2017-09-10 14:14:35 -0500

    + Archivos movidos a /res + Agregado estilo para scroll + Cambiado padding

--- res/js/amatime.js
+++ res/js/amatime.js
@@ -1,3 +1,11 @@
+var days = [];
+for (i = 1; i < 32; i++)
+	days.push(i);
+
+var years = [];
+for (i = 1995; i < 2018; i++)
+	years.push(i);
+
 var Vju = new Vue({
 	el: '#container',
 	data: {
@@ -14,6 +22,8 @@ var Vju = new Vue({
 			'October',
 			'November',
 			'December'
-		]
+		],
+		days: days,
+		years: years
 	}
 });
