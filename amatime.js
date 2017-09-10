4e5a2bb099ad36b1ed9740cc609d1d4c97c48f11
Miguel Amatón <kawatzaki@gmail.com>
2017-09-10 13:04:25 -0500

    Agregados elementos iniciales vacios

---
+++ amatime.js
@@ -0,0 +1,19 @@
+var Vju = new Vue({
+	el: '#container',
+	data: {
+		months: [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'Agoust',
+			'September',
+			'October',
+			'November',
+			'December'
+		]
+	}
+});
